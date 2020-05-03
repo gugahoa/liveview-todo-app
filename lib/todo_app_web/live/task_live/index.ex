af01f1505564b2ce3c5cd10dca168d8c490b1a67
Gustavo Aguiar <gugahoa@gmail.com>
2020-05-03 20:25:16 -0300

    create new list task view

--- lib/todo_app_web/live/task_live/index.ex
+++ lib/todo_app_web/live/task_live/index.ex
@@ -2,7 +2,6 @@ defmodule TodoAppWeb.TaskLive.Index do
   use TodoAppWeb, :live_view
 
   alias TodoApp.Todos
-  alias TodoApp.Todos.Task
 
   @impl true
   def mount(_params, _session, socket) do
@@ -10,34 +9,12 @@ defmodule TodoAppWeb.TaskLive.Index do
   end
 
   @impl true
-  def handle_params(params, _url, socket) do
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-  end
-
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    socket
-    |> assign(:page_title, "Edit Task")
-    |> assign(:task, Todos.get_task!(id))
-  end
-
-  defp apply_action(socket, :new, _params) do
-    socket
-    |> assign(:page_title, "New Task")
-    |> assign(:task, %Task{})
-  end
-
-  defp apply_action(socket, :index, _params) do
-    socket
-    |> assign(:page_title, "Listing Tasks")
-    |> assign(:task, nil)
-  end
-
-  @impl true
-  def handle_event("delete", %{"id" => id}, socket) do
-    task = Todos.get_task!(id)
-    {:ok, _} = Todos.delete_task(task)
-
-    {:noreply, assign(socket, :tasks, fetch_tasks())}
+  def handle_event("toggle-task", %{"_target" => [task_id]}, socket) do
+    with task <- Todos.get_task!(task_id),
+         {:ok, _} <- Todos.update_task(task, %{done: not task.done}),
+         task_list <- fetch_tasks() do
+      {:noreply, assign(socket, :tasks, task_list)}
+    end
   end
 
   defp fetch_tasks do
