f5e5ec45a173cb944ac736ec94b87c423f8b1688
Gustavo Aguiar <gugahoa@gmail.com>
2020-05-03 20:24:33 -0300

    watch lib/todo_app/

--- config/dev.exs
+++ config/dev.exs
@@ -61,7 +61,8 @@ config :todo_app, TodoAppWeb.Endpoint,
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
       ~r"lib/todo_app_web/(live|views)/.*(ex)$",
-      ~r"lib/todo_app_web/templates/.*(eex)$"
+      ~r"lib/todo_app_web/templates/.*(eex)$",
+      ~r"lib/todo_app/.*/.*(ex$)"
     ]
   ]
 
